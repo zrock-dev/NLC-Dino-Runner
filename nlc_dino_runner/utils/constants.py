f64cc657802da685225036c974c01b197b16726f
Rocio Encinas <chio.ref@gmail.com>
2021-09-03 08:40:26 -0400

    Initial commit

--- nlc_dino_runner/utils/constants.py
+++ nlc_dino_runner/utils/constants.py
@@ -2,15 +2,12 @@ import pygame
 import os
 
 # Global Constants
-TITLE = "Chrome Dino Runner"
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 FPS = 30
 IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
 
 # Assets Constants
-ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoWallpaper.png"))
-
 RUNNING = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
